fd41f02737e6d686fad41a7eda00faef3b330d4b
mabbuprem <mabbupremkumar@gmail.com>
2022-06-13 07:52:23 +0530

    [prem] FEATURE ADDED:The Player rolls the die to get a number

--- SnakeAndLadder.php
+++ SnakeAndLadder.php
@@ -1,17 +1,27 @@
 <?php
 class SnakeAndLadder{
+   const NO_PLAY = 0;
+   const SNAKE = 1; 
+   const LADDER = 2;
+
+   public $position = 0;
 
 function welcome(){
 echo "Welcome to the Snake And Ladder Simulator Program";
 }
 
-function startPosition(){
+
+
+function Position(){
     $startPosition = 0;
-    echo "Player started at a $startPosition position";
+    echo "Player started at a $startPosition position\n";
+
+    $diceRoll = rand(1,6);
+    echo "Player Position is  : $diceRoll ";
 }
 
 }
 $position = new SnakeAndLadder();
-$position->startPosition();
+$position->Position();
 
 ?>
