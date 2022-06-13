c040dc905d525c484b577ecd867672075b42879e
mabbuprem <mabbupremkumar@gmail.com>
2022-06-13 07:48:29 +0530

    [prem] FEATURE ADDED:Piayer start position

--- SnakeAndLadder.php
+++ SnakeAndLadder.php
@@ -5,7 +5,13 @@ function welcome(){
 echo "Welcome to the Snake And Ladder Simulator Program";
 }
 
+function startPosition(){
+    $startPosition = 0;
+    echo "Player started at a $startPosition position";
 }
 
+}
+$position = new SnakeAndLadder();
+$position->startPosition();
 
 ?>
