073eaf4fce821dc16b681e142039cd5fa5948a7d
mabbuprem <mabbupremkumar@gmail.com>
2022-06-18 06:17:57 +0530

    [prem] ADDED: play with two Players

--- SnakeAndLadder.php
+++ SnakeAndLadder.php
@@ -1,57 +1,97 @@
 <?php
+
 class SnakeAndLadder
 {
-    const NO_PLAY = 0;
-    const SNAKE = 1;
-    const LADDER = 2;
+
     const START_POSITION = 0;
     const WINNING_POSITION = 100;
 
-    public $position = 0;
-    public $diceCount = 0;
 
-    function welcome()
+    public $diceNum = 0;
+    public $diceCount = 0;
+    public $player;
+    public $position = 0;
+
+    function welcomeMsg()
     {
-        echo "Welcome to the Snake And Ladder Simulator Program";
+        echo "Welcome to snake and ladder game \n";
     }
 
-
-
-    function Position()
+    //calling the switch case function seperately for multiple users usage
+    function diceRoll()
     {
+        //Random function to rolling dice
+        $this->diceNum = rand(1, 6);
+        echo "The dice number is : " . $this->diceNum . "\n";
 
-        echo "Player started at " . SnakeAndLadder::START_POSITION . " position\n ";
-        while ($this->position < SnakeAndLadder::WINNING_POSITION) {
-            $this->diceCount++;
-            $diceRoll = rand(1, 6);                   // random function to rolling a dice
-            echo "\nDice Rolled  to : $diceRoll \n";
-            $option = rand(0, 2);                       // options to play
-            if ($option == SnakeAndLadder::SNAKE) {
+        //for choosing options randomly
+        $options = rand(0, 2);
+        switch ($options) {
+            case 0:
+                echo "$this->player has got a no play!!\n$this->player current position is " . $this->position . "\n";
+                $this->position = $this->position;
+                break;
 
-                if ($this->position < 0) {
-                    $this->position = SnakeAndLadder::START_POSITION; //if player moves below 0, starts from 0.
-                } else {
-                    $this->position -= $diceRoll;
-                    echo "Snake bite at $this->position ";
+            case 1:
+                echo "$this->player  Snake bite ?? !\n";
+                $this->position -= $this->diceNum;
+
+                if ($this->position < SnakeAndLadder::START_POSITION) {
+                    $this->position += $this->diceNum;
                 }
-            } elseif ($option == SnakeAndLadder::LADDER) {
-                //echo "Ladder\n";
-                $this->position += $diceRoll;
+                echo "$this->player current position is " . $this->position . "\n";
+
+                break;
+
+            case 2:
+                echo "$this->player Ladder Reached---\n$this->player can move up $this->diceNum numbers.\n";
+
+                $this->position += $this->diceNum;
+
                 if ($this->position > SnakeAndLadder::WINNING_POSITION) {
-                    echo " Roll Again Till You Reach 100 Exactly \n ";
-                    $this->position -= $diceRoll;         // if player reaches above 100, will stay at same position
+                    $this->position -= $this->diceNum;
+                    echo "$this->player current position is " . $this->position . "\n";
                 } else {
-                    echo "Ladder option at $this->position ";
+                    echo "$this->player can roll the die again\n";
+                    $this->diceRoll();
                 }
-            } else {
+                break;
+        }
+    }
 
-                echo "No play at $this->position ";
+    function gamePlay()
+    {
+        // $this->position = 0;
+
+        while ($this->position != SnakeAndLadder::WINNING_POSITION) {
+            $this->diceCount++;
+            echo "Dice count :" . $this->diceCount . "\n";
+
+
+            //looping the game with 2 players                               
+            for ($i = 1; $i <= 2; $i++) {
+                if ($i == 1) {
+                    $this->player = "player1";
+                    echo "$this->player is playing\n";
+                    $this->diceRoll();
+                    echo "\n";
+                } elseif ($this->position == SnakeAndLadder::WINNING_POSITION) {
+                    break;
+                } else {
+                    $this->player = "player2";
+                    echo "$this->player is playing\n";
+                    $this->diceRoll();
+                }
+            }
+
+            if ($this->position == SnakeAndLadder::WINNING_POSITION) {
+                echo "$this->player has reached position: $this->position.\n$this->player has won the game!!\n";
+                echo "\nThe number of times the dice rolled is " . $this->diceCount . ". \n";
+                break;
             }
         }
-        echo "\nPlayer current position at $this->position";
-        echo "\nPlayer won a game ";
-        echo "\n Dice Rolled $this->diceCount times to complete this game ";
     }
 }
-$position = new SnakeAndLadder();
-$position->Position();
+$play = new SnakeAndLadder();
+$play->diceRoll();
+$play->gamePlay();
